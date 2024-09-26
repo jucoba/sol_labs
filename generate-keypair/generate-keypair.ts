8667ee3dcc2618002e71cc6d946aa0cfba7e2a7d
Jubel Correa <jubel.correa@gmail.com>
2024-09-26 11:59:49 -0500

    Failing to load secret key from env

---
+++ generate-keypair/generate-keypair.ts
@@ -0,0 +1,8 @@
+import { addKeypairToEnvFile } from "@solana-developers/helpers";
+import { Keypair } from "@solana/web3.js";
+
+const keypair = Keypair.generate();
+await addKeypairToEnvFile(keypair, "SECRET_KEY");
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`);
