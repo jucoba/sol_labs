ef51c67bd5be4b28cec1a44c40e937eee84ff74a
Jubel Correa <jubel.correa@gmail.com>
2024-10-01 20:59:14 -0500

    counter program

--- anchor-counter/programs/anchor-counter/src/lib.rs
+++ anchor-counter/programs/anchor-counter/src/lib.rs
@@ -7,10 +7,40 @@ pub mod anchor_counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter account created");
+        msg!("Current count:{ }", counter.count);
         Ok(())
     }
+
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Current count:{ }", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented: New count:{ }", counter.count);
+        Ok(())
+    }
+
+}
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
+
